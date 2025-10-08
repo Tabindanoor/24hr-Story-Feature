34a603d624fd0b1feff6bf928e7b3e2e315dcfaf
Tabindanoor <tabindanoor415@gmail.com>
2025-10-08 22:01:16 +0500

    help needed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Stories from "@/components/Stories";
 export default function Home() {
   return (
     <div >
-       <main className="min-h-screen bg-gray-100 p-6">
+       <div className="min-h-screen bg-gray-100 p-6">
       <h1 className="text-2xl font-bold text-center mb-4">My Stories</h1>
       <Stories />
-    </main>
+    </div>
     </div>
   );
 }
